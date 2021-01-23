afe5bd235fc45b5ca45b000e100d0770894f3cb2
ncavchl <ncavchl@ajou.ac.kr>
2021-01-23 22:51:02 +0900

    dto test

--- src/test/java/com/ncavchl/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ncavchl/book/springboot/web/HelloControllerTest.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 
 @RunWith(SpringRunner.class)
@@ -18,13 +22,32 @@ public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
+    //웹 api 테스트 할때 사용
 
     @Test
     public void hello가_리턴된다() throws Exception{
         String hello = "hello";
 
+        //MockMvc를 사용하여 /hello 주소로 http Get 요청함
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+                //andExpect - mvc.perform의 결과를 검증함
+                //status() http 헤더 상태를 검증 200,404,500 그런거.. OK는 200
+                //content() 응답 본문 내용 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+                //jsonPath json 응답값을 필드별로 검증하는 메소드 , $를 기준으로 필드명 명시
     }
 }
